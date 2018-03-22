2d6b382970fdd00c82a65626146cc688646bfe87
Matthew Bloomfield <matthewbloomfield@WDCMW2962.local>
2018-03-22 14:57:47 -0400

    Fixing some piping/reloading

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     del = require('del');
 
+    var basepath = 'public';
+
 var styles = ['src/app/scss/style.scss', 'src/pages/**/*.scss'];
 
 gulp.task('sass', function () {
@@ -17,11 +19,12 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', function() {
         notify({ message: 'Sass failed to compile'})
     }))
-    .pipe(gulp.dest('public/bin/css'))
+    .pipe(gulp.dest(basepath + '/bin/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(cssnano())
-    .pipe(gulp.dest('public/bin/css'))
-    .pipe(notify({ message: 'Sass task complete' }));
+    .pipe(gulp.dest(basepath + '/bin/css'))
+    .pipe(notify({ message: 'Sass task complete' }))
+    .pipe(livereload());
 });
 
 var javascripts = ['src/app/js/app.js', 'src/app/js/services/*.js', 'src/pages/**/*.js'];
@@ -29,22 +32,31 @@ var javascripts = ['src/app/js/app.js', 'src/app/js/services/*.js', 'src/pages/*
 gulp.task('scripts', function() {
   return gulp.src(javascripts)
     .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('public/bin/js'))
+    .pipe(gulp.dest(basepath + '/bin/js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('public/bin/js'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(gulp.dest(basepath + '/bin/js'))
+    .pipe(notify({ message: 'Scripts task complete' }))
+    .pipe(livereload());
 });
 
 var htmls = ['src/*.html', 'src/pages/**/*.html'];
 
 gulp.task('copyHtml', function() {
   return gulp.src(htmls)
-  .pipe(gulp.dest('public'));
+  .pipe(gulp.dest(basepath))
+  .pipe(livereload());
 });
 
 gulp.task('watch', function() {
+  livereload.listen({ basePath: basepath });
   gulp.watch(styles, ['sass']);
-  gulp.watch(javascripts, ['scripts']);
+  gulp.watch(javascripts, ['scripts'])
   gulp.watch(htmls, ['copyHtml']);
+});
+
+gulp.task('server', function(done) {
+  http.createServer(
+    st({ path: __dirname + '/' + basepath, index: 'index.html', cache: false })
+  ).listen(8080, done);
 });
